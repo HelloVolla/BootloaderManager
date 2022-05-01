9323b35c83be167cbd20287173e6ad5f9a1b4ac6
Erfan Abdi <erfangplus@gmail.com>
2022-05-01 12:23:37 +0430

    Home: Check for metadata on SDCard p1

--- app/src/main/java/org/androidbootmanager/app/legacy/ui/home/HomeFragment.java
+++ app/src/main/java/org/androidbootmanager/app/legacy/ui/home/HomeFragment.java
@@ -1,5 +1,7 @@
 package org.androidbootmanager.app.legacy.ui.home;
 
+import static org.androidbootmanager.app.legacy.util.SDUtils.generateMeta;
+
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -27,11 +29,13 @@ import org.androidbootmanager.app.legacy.ui.activities.MainActivity;
 import org.androidbootmanager.app.legacy.ui.installer.InstallerWelcomeWizardPageFragment;
 import org.androidbootmanager.app.legacy.ui.wizard.WizardActivity;
 import org.androidbootmanager.app.legacy.util.Constants;
+import org.androidbootmanager.app.legacy.util.SDUtils;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class HomeFragment extends Fragment {
 
@@ -52,11 +56,11 @@ public class HomeFragment extends Fragment {
         AtomicBoolean check3 = new AtomicBoolean(false);
         AtomicBoolean sd = new AtomicBoolean(false);
         ImageView statusImg = root.findViewById(R.id.home_installedWorking_image);
-            check0.set(true);
+            check0.set(true); // Root
             statusText0.setText(check0.get() ? R.string.ok : R.string.failure);
-            check1.set(true);
+            check1.set(true); // Script pass
             statusText1.setText(check1.get() ? R.string.ok : R.string.failure);
-            check2.set(true);
+            check2.set(true); // Has ABM bootloader
             statusText2.setText(check2.get() ? R.string.ok : R.string.failure);
             check3.set(SuFile.open("/data/abm/codename.cfg").exists());
             statusText3.setText(check3.get() ? R.string.ok : R.string.failure);
@@ -83,14 +87,21 @@ public class HomeFragment extends Fragment {
             }
             if (!model.getCodename().getValue().isEmpty())
                 sd.set(SuFile.open(DeviceList.getModel(model).bdev).exists());
+
+            if (sd.get()) {
+                AtomicReference<SDUtils.SDPartitionMeta> meta = new AtomicReference<>(generateMeta(DeviceList.getModel(model)));
+                if (!meta.get().p.get(0).code.equals("8301"))
+                    Toast.makeText(requireActivity(), "Bad metadata", Toast.LENGTH_LONG).show();
+                else
+                    check3.set(true);
+            }
             installButton.setOnClickListener((v) -> startActivity(new Intent(requireActivity(), WizardActivity.class).putExtra("StartFragment", InstallerWelcomeWizardPageFragment.class)));
             ((NavigationView) requireActivity().findViewById(R.id.nav_view)).getMenu().findItem(R.id.nav_generalcfg).setEnabled(check1.get() && check2.get() && check3.get());
-            ((NavigationView) requireActivity().findViewById(R.id.nav_view)).getMenu().findItem(R.id.nav_roms).setEnabled(check1.get() && check2.get() && check3.get());
-            ((NavigationView) requireActivity().findViewById(R.id.nav_view)).getMenu().findItem(R.id.nav_sd).setEnabled(check1.get() && check2.get() && check3.get());
+            ((NavigationView) requireActivity().findViewById(R.id.nav_view)).getMenu().findItem(R.id.nav_roms).setEnabled(check2.get() && check3.get() && sd.get());
+            ((NavigationView) requireActivity().findViewById(R.id.nav_view)).getMenu().findItem(R.id.nav_sd).setEnabled(check2.get() && sd.get());
             if (check1.get() && check2.get() && check3.get()) {
                 if (!((MainActivity) requireActivity()).mount(DeviceList.getModel(model)))
                     Toast.makeText(requireActivity(), R.string.bootset_fail, Toast.LENGTH_LONG).show();
-                ((NavigationView) requireActivity().findViewById(R.id.nav_view)).getMenu().findItem(R.id.nav_sd).setEnabled(sd.get());
             }
         return root;
     }
